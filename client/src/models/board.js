6e966abd7be3a3be9d4b32cb942d0b58c0ea7618
Alison Babington Smith <a.babingtonsmith@gmail.com>
2018-09-21 14:27:49 +0100

    board model created

---
+++ client/src/models/board.js
@@ -0,0 +1,13 @@
+const Board = function () {
+
+};
+
+Board.prototype.movesPlayer = function (player, dieAmount) {
+  for (i = 0, i < dieAmount, i++)
+   player.position += 1;
+   if (player.position === 26) {
+     player.position = 0;
+   };
+};
+
+module.exports = Board;
