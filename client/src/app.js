3e762185e2b0850e66a27b1c2d8a34e8a363d437
Alison Babington Smith <a.babingtonsmith@gmail.com>
2018-09-23 11:32:42 +0100

    start move player method

--- client/src/app.js
+++ client/src/app.js
@@ -6,19 +6,23 @@ const BoardView = require('./views/board_view.js');
 
 document.addEventListener('DOMContentLoaded', () => {
 
-const card = new Card('')
-card.getData();
+// const card = new Card('')
+// card.getData();
 
 // const container = document.querySelector('#card-details');
 // const quizView = new QuizView(container, form);
 // quizView.bindEvents();
 
 const player1 = new Player("player1");
+const player2 = new Player("player2")
+const game = new Game(player1, player2)
 
-const boardView = new BoardView();
+const boardView = new BoardView(game);
 boardView.bindEvents();
 
 const board = new Board();
 board.movesPlayer(player1,3);
+
+
 
 });
--- client/src/views/board_view.js
+++ client/src/views/board_view.js
@@ -1,120 +1,144 @@
+const createAndAppend = require('../helpers/create_append');
 const PubSub = require('../helpers/pub_sub.js');
 
-const BoardView = function() {
-  this.playerPiece = document.querySelector('#player-piece');
-  this.playerPosition = 0;
-};
+const BoardView = function (game) {
+  this.game = game;
+  this.player1Piece = null;
+  this.player2Piece = null;
+}
 
 BoardView.prototype.bindEvents = function () {
-  this.setupBoard();
-  PubSub.subscribe('Board:move-player', (evt) => {
-    const moveDetails = evt.detail;
-    this.forward(moveDetails);
-  });
+  const start = document.querySelector('#space0')
+  this.player1Piece = createAndAppend('div', 'playerPiece', 'player1', '', start)
+  this.player2Piece = createAndAppend('div', 'playerPiece', 'player2', '', start)
+  this.movePlayer('#space0');
 };
 
-BoardView.prototype.setupBoard = function () {
-  this.playerPiece.style.top = 0;
-  this.playerPiece.style.left = 0;
+BoardView.prototype.movePlayer = function (start) {
+    PubSub.subscribe('Board:move-player', (evt) => {
+      const player = evt.detail;
+      const position = evt.detail.position;
+
+      const place = document.querySelector(`${start}`)
+      console.log(place);
+      place.removeChild(this.player1Piece)
+      document.querySelector(`#space${position}`).appendChild(this.player1Piece);
+    });
+
 };
 
-BoardView.prototype.moveDown = function () {
-    currentPositionY = this.playerPiece.style.top.slice(0, -2)
-    currentPositionY = parseInt(currentPositionY);
-    if (currentPositionY < 300) {
-      currentPositionY += 50;
-    }
-    this.playerPiece.style.top = currentPositionY + 'px';
-  };
-BoardView.prototype.moveUp = function () {
-    currentPositionY = this.playerPiece.style.top.slice(0, -2)
-    currentPositionY = parseInt(currentPositionY);
-    if (currentPositionY > 0) {
-      currentPositionY -= 50;
-    }
-    this.playerPiece.style.top = currentPositionY + 'px';
-  };
-BoardView.prototype.moveLeft = function () {
-    currentPositionX = this.playerPiece.style.left.slice(0, -2)
-    currentPositionX = parseInt(currentPositionX);
-    if (currentPositionX > 0) {
-      currentPositionX -= 50;
-    }
-    this.playerPiece.style.left = currentPositionX + 'px';
-  };
-BoardView.prototype.moveRight = function () {
-    currentPositionX = this.playerPiece.style.left.slice(0, -2);
-    currentPositionX = parseInt(currentPositionX);
-    if (currentPositionX < 300) {
-      currentPositionX += 50;
-    }
-    this.playerPiece.style.left = currentPositionX + 'px';
-  };
 
-BoardView.prototype.forward = function (number_of_squares) {
-let moves = number_of_squares;
-while (moves>0) {
-  while (this.playerPosition<6) {
-    this.moveRight();
-    this.playerPosition++;
-    moves--;
-    if (moves <= 0) break;
-  };
-  while (this.playerPosition>5 && this.playerPosition<12) {
-    if (moves <= 0) break;
-    this.moveDown();
-    this.playerPosition++;
-    moves--;
-  };
-  while (this.playerPosition>11 && this.playerPosition<18) {
-    if (moves <= 0) break;
-    this.moveLeft();
-    this.playerPosition++;
-    moves--;
-  };
-  while (this.playerPosition>17 && this.playerPosition<24) {
-    if (moves <= 0) break;
-    this.moveUp();
-    this.playerPosition++;
-    moves--;
-    if (this.playerPosition === 24) {
-      this.playerPosition = 0;
-    }
-  };
-};
-}
-
-BoardView.prototype.backward = function (number_of_squares) {
-let moves = number_of_squares;
-while (moves>0) {
-while (this.playerPosition>17 && this.playerPosition<24) {
-  if (moves <= 0) break;
-  this.moveDown();
-  this.playerPosition--;
-  moves--;
-};
-while (this.playerPosition>11 && this.playerPosition<18) {
-  if (moves <= 0) break;
-  this.moveRight();
-  this.playerPosition--;
-  moves--;
-};
-while (this.playerPosition>5 && this.playerPosition<12) {
-  if (moves <= 0) break;
-  this.moveUp();
-  this.playerPosition--;
-  moves--;
-};
-while (this.playerPosition<6) {
-  if (moves <= 0) break;
-  this.moveLeft();
-  this.playerPosition--;
-  moves--;
-  if (this.playerPosition === -1) {
-    this.playerPosition = 23;
-  }
-};
-};
-}
+// const BoardView = function(game) {
+//   this.playerPiece = document.querySelector('#player-piece');
+//   this.playerPosition = 0;
+// };
+//
+// BoardView.prototype.bindEvents = function () {
+//   this.setupBoard();
+// };
+//
+// BoardView.prototype.setupBoard = function () {
+//   this.playerPiece.style.top = 0;
+//   this.playerPiece.style.left = 0;
+// };
+//
+// BoardView.prototype.moveDown = function () {
+//     currentPositionY = this.playerPiece.style.top.slice(0, -2)
+//     currentPositionY = parseInt(currentPositionY);
+//     if (currentPositionY < 300) {
+//       currentPositionY += 50;
+//     }
+//     this.playerPiece.style.top = currentPositionY + 'px';
+//   };
+// BoardView.prototype.moveUp = function () {
+//     currentPositionY = this.playerPiece.style.top.slice(0, -2)
+//     currentPositionY = parseInt(currentPositionY);
+//     if (currentPositionY > 0) {
+//       currentPositionY -= 50;
+//     }
+//     this.playerPiece.style.top = currentPositionY + 'px';
+//   };
+// BoardView.prototype.moveLeft = function () {
+//     currentPositionX = this.playerPiece.style.left.slice(0, -2)
+//     currentPositionX = parseInt(currentPositionX);
+//     if (currentPositionX > 0) {
+//       currentPositionX -= 50;
+//     }
+//     this.playerPiece.style.left = currentPositionX + 'px';
+//   };
+// BoardView.prototype.moveRight = function () {
+//     currentPositionX = this.playerPiece.style.left.slice(0, -2);
+//     currentPositionX = parseInt(currentPositionX);
+//     if (currentPositionX < 300) {
+//       currentPositionX += 50;
+//     }
+//     this.playerPiece.style.left = currentPositionX + 'px';
+//   };
+//
+// BoardView.prototype.forward = function (number_of_squares) {
+// let moves = number_of_squares;
+// while (moves>0) {
+//   while (this.playerPosition<6) {
+//     this.moveRight();
+//     this.playerPosition++;
+//     moves--;
+//     if (moves <= 0) break;
+//   };
+//   while (this.playerPosition>5 && this.playerPosition<12) {
+//     if (moves <= 0) break;
+//     this.moveDown();
+//     this.playerPosition++;
+//     moves--;
+//   };
+//   while (this.playerPosition>11 && this.playerPosition<18) {
+//     if (moves <= 0) break;
+//     this.moveLeft();
+//     this.playerPosition++;
+//     moves--;
+//   };
+//   while (this.playerPosition>17 && this.playerPosition<24) {
+//     if (moves <= 0) break;
+//     this.moveUp();
+//     this.playerPosition++;
+//     moves--;
+//     if (this.playerPosition === 24) {
+//       this.playerPosition = 0;
+//     }
+//   };
+// };
+// }
+//
+// BoardView.prototype.backward = function (number_of_squares) {
+// let moves = number_of_squares;
+// while (moves>0) {
+// while (this.playerPosition>17 && this.playerPosition<24) {
+//   if (moves <= 0) break;
+//   this.moveDown();
+//   this.playerPosition--;
+//   moves--;
+// };
+// while (this.playerPosition>11 && this.playerPosition<18) {
+//   if (moves <= 0) break;
+//   this.moveRight();
+//   this.playerPosition--;
+//   moves--;
+// };
+// while (this.playerPosition>5 && this.playerPosition<12) {
+//   if (moves <= 0) break;
+//   this.moveUp();
+//   this.playerPosition--;
+//   moves--;
+// };
+// while (this.playerPosition<6) {
+//   if (moves <= 0) break;
+//   this.moveLeft();
+//   this.playerPosition--;
+//   moves--;
+//   if (this.playerPosition === -1) {
+//     this.playerPosition = 23;
+//   }
+// };
+// };
+// }
 
 module.exports = BoardView;
