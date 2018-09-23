24febde4f5472677354cc090edf3ecb63905e9b7
Alison Babington Smith <a.babingtonsmith@gmail.com>
2018-09-23 11:31:18 +0100

    added id as possible input

--- client/src/helpers/create_append.js
+++ client/src/helpers/create_append.js
@@ -1,7 +1,8 @@
-const createAndAppend = function (tag, htmlclass, text, parent) {
+const createAndAppend = function (tag, htmlclass, id, text, parent) {
  const element = document.createElement(tag);
  element.textContent = text;
- if (htmlclass) { element.classList.add(htmlclass) }
+ if (htmlclass) { element.classList.add(htmlclass) };
+ if (id) {element.id = id};
  parent.appendChild(element);
 
  return element;
