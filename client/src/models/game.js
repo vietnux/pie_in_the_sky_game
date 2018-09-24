47e0e8f8e74e744cdcaa1a8f740461c4c1f4f57d
Alison Babington Smith <a.babingtonsmith@gmail.com>
2018-09-24 18:45:04 +0100

    timer in place but stuck at 1.59

--- client/src/models/game.js
+++ client/src/models/game.js
@@ -22,11 +22,33 @@ Game.prototype.startGame = function () {
 
 Game.prototype.playTurn = function () {
   PubSub.subscribe('Player:rollnumber', (event) => {
+  this.timer(2);
   const moves = event.detail;
   console.log('moves', moves);
   this.board.movesPlayer(this.currentPlayer, moves);
 });
 
+};
+
+Game.prototype.timer = function (time) {
+  let seconds = 60;
+  let mins = time;
+
+  if( seconds > 0 ) {
+    setTimeout(this.timerCountdown(mins, seconds), 1000);
+  }
+  else if (mins > 1){
+    countdown(mins-1);
+  }
+  this.timerCountdown(mins, seconds);
+};
+
+
+Game.prototype.timerCountdown = function (mins, seconds) {
+  const timer = document.querySelector('#timer');
+  let currentMins = mins - 1
+  seconds --;
+  timer.innerHTML = currentMins.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
 };
 
 Game.prototype.checkResult = function (result) {
